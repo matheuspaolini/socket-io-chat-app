6a91900bb0a31ae32e305f1819b76b5b0227ab34
Matheus <dev.paolini.matheus@gmail.com>
2023-04-29 12:44:33 -0300

    added chat by id page

---
+++ client/pages/chat/[chatId].tsx
@@ -0,0 +1,11 @@
+import { Box, Button, Input, Space, Stack, Title } from '@mantine/core';
+
+export default function ChatByIdPage() {
+  return (
+    <Box>
+      <Title>Chat By Id Page</Title>
+
+      <Space h={32} />
+    </Box>
+  );
+}
